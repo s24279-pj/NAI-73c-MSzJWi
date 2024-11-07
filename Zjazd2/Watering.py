dff9efb2d56b50ba550e32f62ef7d974ce7387f5
s24256-pj <martaszpilka@MacBook-Air-Marta.local>
2024-11-07 16:18:54 +0100

    Poprawki

--- Zjazd2/Watering.py
+++ Zjazd2/Watering.py
@@ -21,8 +21,8 @@ time = ctrl.Consequent(np.arange(0, 30, 1), 'time')
 
 # Definicja funkcji przynależności dla czasu podlewania
 time['short'] = fuzz.trimf(time.universe, [0,0,10])
-time['medium'] = fuzz.trimf(time.universe, [0,10,20])
-time['long'] = fuzz.trimf(time.universe, [10,20,30])
+time['medium'] = fuzz.trimf(time.universe, [5,15,25])
+time['long'] = fuzz.trimf(time.universe, [20,30,30])
 
 # Wizualizacja funkcji przynależności
 time.view()
@@ -33,7 +33,7 @@ temperature.view()
 rule1 = ctrl.Rule(humidity_soil['poor'] | humidity_air['poor'], time['long'])
 rule2 = ctrl.Rule(humidity_air['good'] & temperature['optimal'], time['short'])
 rule3 = ctrl.Rule(humidity_soil['poor'] | humidity_air['good'] | temperature['optimal'], time['medium'])
-rule4 = ctrl.Rule(humidity_soil['good'] & humidity_air['good'] | temperature['cold'], time['short'])
+rule4 = ctrl.Rule(humidity_soil['good'] & humidity_air['good'], time['short'])
 rule5 = ctrl.Rule(humidity_soil['average'] & humidity_air['average'] & temperature['optimal'], time['short'])
 rule6 = ctrl.Rule(humidity_soil['poor'] & temperature['hot'], time['long'])
 rule7 = ctrl.Rule(humidity_soil['good'] & temperature['cold'], time['short'])
@@ -41,18 +41,20 @@ rule8 = ctrl.Rule(humidity_air['poor'] & (temperature['optimal'] | temperature['
 rule9 = ctrl.Rule(humidity_air['good'] & temperature['optimal'] & humidity_soil['good'], time['short'])
 rule10 = ctrl.Rule(humidity_soil['poor'] & humidity_air['average'] & temperature['cold'], time['medium'])
 rule11 = ctrl.Rule(humidity_soil['average'] & humidity_air['average'] & temperature['hot'], time['medium'])
-rule12 = ctrl.Rule(humidity_soil['good'] & humidity_air['poor'] & temperature['hot'], time['long'])
+rule12 = ctrl.Rule(humidity_soil['good'] & humidity_air['poor'] & temperature['hot'], time['medium'])
+rule13 = ctrl.Rule(humidity_soil['poor'] & humidity_air['poor'] & temperature['hot'], time['long'])
+rule14 = ctrl.Rule((humidity_soil['poor'] | humidity_air['poor']) & temperature['hot'], time['long'])
 
 # Tworzenie systemu sterowania i symulacji
 watering_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11,
-                                    rule12])
+                                    rule12, rule13, rule14])
 
 watering = ctrl.ControlSystemSimulation(watering_ctrl)
 
 #Przykładowe dane wejściowe
-watering.input['humidity air'] = 40
-watering.input['humidity soil'] = 34
-watering.input['temperature'] = 20
+watering.input['humidity air'] = 0
+watering.input['humidity soil'] = 0
+watering.input['temperature'] = 35
 
 # Przeprowadzenie obliczeń
 watering.compute()
