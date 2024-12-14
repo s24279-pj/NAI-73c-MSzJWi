686338cd9034a7a66b5ad7ee7e47b3ab6249d113
s24256-pj <martaszpilka@MacBook-Air-Marta.local>
2024-12-14 12:37:12 +0100

    Add second model to heart disease, add model for clothes

--- Zjazd5/heart_disease.py
+++ Zjazd5/heart_disease.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import tensorflow as tf
+import numpy as np
 
 def main():
+
     """
        Wczytuje dane z pliku CSV, ustawia odpowiednie nagłówki dla kolumn.
     """
@@ -25,10 +27,10 @@ def main():
 
     """
         Podział danych na zbiory treningowy i testowy:
-       Ustal proporcję podziału danych, np. 80% dla treningu, 20% dla testu.
+       Ustal proporcję podziału danych, np. 70% dla treningu, 30% dla testu.
     """
-    train_size = int(0.8 * len(X))  # 80% dla treningu
-    test_size = len(X) - train_size  # 20% dla testu
+    train_size = int(0.7 * len(X))  # 70% dla treningu
+    test_size = len(X) - train_size  # 30% dla testu
 
     X_train, y_train = X.iloc[:train_size], y[:train_size]
     X_test, y_test = X.iloc[train_size:], y[train_size:]
@@ -37,34 +39,54 @@ def main():
        Tworzy model sieci neuronowej z dwoma warstwami Dense, Dropout dla regularizacji
        oraz warstwą wyjściową z funkcją aktywacji 'sigmoid' dla klasyfikacji binarnej.
     """
-    model = tf.keras.models.Sequential([
+    model1 = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    model2 = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dropout(0.1),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.1),
-        tf.keras.layers.Dense(64, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
     """
        Tworzy optymalizator Adam z określoną stopą uczenia.
     """
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+    optimizer2 = tf.keras.optimizers.Adam(learning_rate=0.001)
 
     """
        Kompiluje model z funkcją straty 'binary_crossentropy' oraz metryką 'accuracy'.
     """
-    model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model1.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+    model2.compile(optimizer=optimizer2, loss='binary_crossentropy', metrics=['accuracy'])
 
     """
        Trenuje model na danych treningowych przez 50 epok z wykorzystaniem
        zbioru testowego do walidacji.
     """
-    model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+    model1.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+    loss, accuracy = model1.evaluate(X_test, y_test)
+    print(f"Model 1 - Loss: {loss}, Accuracy: {accuracy}")
+
+    model2.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
 
     """
         Ocena modelu na danych testowych. Zwracana jest strata oraz dokładność.
     """
-    loss, accuracy = model.evaluate(X_test, y_test)
     print(f"Model 1 - Loss: {loss}, Accuracy: {accuracy}")
+
+    loss2, accuracy2 = model2.evaluate(X_test, y_test)
+    print(f"Model 2 - Loss: {loss2}, Accuracy: {accuracy2}")
+
+    y_pred = (model1.predict(X_test) > 0.5).astype(int).flatten()
 
 if __name__ == "__main__":
     main()
