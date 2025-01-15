38b3aac28f9decd1f98e6941dec1332b04bb430a
JakubW-k <jakub.wiecek@kainos.com>
2025-01-15 16:41:41 +0100

    added authors

--- Zjazd6/presentation_hand_gesture.py
+++ Zjazd6/presentation_hand_gesture.py
@@ -1,7 +1,13 @@
+#####################################
+#                                   #
+#   Marta Szpilka, Jakub Więcek     #
+#                                   #
+#####################################
+
 import cv2
 import mediapipe as mp
 import pyautogui
-import time  # Importujemy bibliotekę time
+import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
